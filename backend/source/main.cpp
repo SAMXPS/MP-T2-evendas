318b08011e65f4ec43ec48866f687c0927a35cdc
Leonam S. Gomes <leonamcsg@hotmail.com>
2020-11-23 00:18:30 -0500

    Corrigindo erro no modulo Json.

--- backend/source/main.cpp
+++ backend/source/main.cpp
@@ -4,34 +4,31 @@
 #include "../headers/test.h"
 
 std::string verifica_senha(Json::Value usuario, Json::Value senha) {
-    if(usuario.compare("samuel")&&senha.compare("12345")) {
+    if(usuario.compare("samuel") == 0 && senha.compare("12345") == 0) {
         return "123456789";
     }
 }
 
 int main() {
     Json::Value request;
+    std::string chave;
     Json::Value response;
     Json::Reader reader;
-    Json::FastWriter writer;
 
     bool error = reader.parse(std::cin, request); // Leitura do pedido em json, na entrada padrão.
-    response["status"] = "FALSE";
     if (!error) {
         std::cout << "Error: " << reader.getFormattedErrorMessages();
         response["status"] = "FALSE";
     }
-    
 
     // processa a ação da requisição
-    std::string action = writer.write(request["action"]);
-    std::string chave;
-    if(action.compare("verificarSenha")) { // Verificar senha
-       chave = verifica_senha(request["usuario"], request["senha"]);
+    std::string action = request["data"].get("action","vazio").asString();
+    
+    if(action.compare("verificarSenha") == 0) { // efetua verificção de senha
+       chave = verifica_senha(request["data"]["usuario"], request["data"]["senha"]);
        response["status"] = "OK";
     }
 
-    
     response["session"] = request["session"];
     response["data"]["chave_padrao"] = chave;
     std::cout << response;
