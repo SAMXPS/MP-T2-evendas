d27dfb985f8a4830e48e355855daea35a8305dc8
Leonam S. Gomes <leonamcsg@hotmail.com>
2020-11-22 19:42:21 -0500

    Criação de um protótipo da API REST

--- backend/source/main.cpp
+++ backend/source/main.cpp
@@ -1,7 +1,39 @@
-#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <jsoncpp/json/json.h>
 #include "../headers/test.h"
 
+std::string verifica_senha(Json::Value usuario, Json::Value senha) {
+    if(usuario.compare("samuel")&&senha.compare("12345")) {
+        return "123456789";
+    }
+}
+
 int main() {
-    printf(HELLO);
+    Json::Value request;
+    Json::Value response;
+    Json::Reader reader;
+    Json::FastWriter writer;
+
+    bool error = reader.parse(std::cin, request); // Leitura do pedido em json, na entrada padrão.
+    response["status"] = "FALSE";
+    if (!error) {
+        std::cout << "Error: " << reader.getFormattedErrorMessages();
+        response["status"] = "FALSE";
+    }
+    
+
+    // processa a ação da requisição
+    std::string action = writer.write(request["action"]);
+    std::string chave;
+    if(action.compare("verificarSenha")) { // Verificar senha
+       chave = verifica_senha(request["usuario"], request["senha"]);
+       response["status"] = "OK";
+    }
+
+    
+    response["session"] = request["session"];
+    response["data"]["chave_padrao"] = chave;
+    std::cout << response;
     return 0;
 }
