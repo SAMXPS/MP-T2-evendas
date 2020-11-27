7bafe0ec9a9485775a942266f17fa897076993c6
Leonam S. Gomes <leonamcsg@hotmail.com>
2020-11-27 15:11:01 -0500

    Criando prototipo dos testes gtest.

--- backend/headers/JsonModule.h
+++ backend/headers/JsonModule.h
@@ -8,7 +8,7 @@
  */
 namespace JsonModule{
     /**
-     * @brief verifica usuário e senha
+     * @brief verifica usuário e senha.
      * 
      * Verifica se usuário esta cadastrado e se sua senha confere.
      * 
@@ -18,7 +18,27 @@ namespace JsonModule{
      */
 
     std::string verifyLogin(Json::Value usuario, Json::Value senha);
+
+    /**
+     * @brief Interface de interação do backend.
+     * 
+     * Realiza a interação entre o frontend e o modulo de banco de dados
+     * 
+     * @param input 
+     * @return Json::Value 
+     */
     Json::Value BackendInterface(const std::string&input);
+
+    /**
+     * @brief Registra um usuário
+     * 
+     * Registra um usuario com algumas informações.
+     * 
+     * @param usuario 
+     * @param senha 
+     * @param email 
+     * @return std::string 
+     */
     std::string registerUser(Json::Value usuario, Json::Value senha, Json::Value email);
 }
 #define _JSON_MODULE
--- backend/source/Test.cpp
+++ backend/source/Test.cpp
@@ -1,5 +1,34 @@
-// TODO: Codificar, neste arquivo, todo o ambiente do Gtest
+#include <gtest/gtest.h> // inclui biblioteca de teste gtest.
+#include <JsonModule.h>
 
-int main() {
-    return 0;
+
+/**
+ * @brief Constroi novo objeto de teste do gtest.
+ * 
+ * Teste a função verifyLogin do modulo JsonModule.
+ * 
+ */
+GTEST_TEST(JsonModuleTest, verifyLogin) {
+    Json::Value usuario = "teste@teste.com";
+    Json::Value senha = "teste";
+
+    GTEST_ASSERT_EQ("VALID_PASSWORD", JsonModule::verifyLogin(usuario, senha));
+    usuario = "wrongteste@teste.com";
+    senha = "wrongpassword";
+    GTEST_ASSERT_EQ("INVALID_PASSWORD", JsonModule::verifyLogin(usuario, senha));
+
+}
+
+/**
+ * @brief Executa os teste
+ * 
+ * Prepara os testes do gtest e os executam.
+ * 
+ * @param argc 
+ * @param argv 
+ * @return int 
+ */
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
