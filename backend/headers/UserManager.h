74630f25b047ebb1022fa3b88706b7cb6f6c6aa5
Samuel James <bsbcraftplays@gmail.com>
2020-12-12 09:10:58 -0500

    Simplificando UserManager

--- backend/headers/UserManager.h
+++ backend/headers/UserManager.h
@@ -1,80 +1,42 @@
-#ifndef _USER_MANAGER
-#define _USER_MANAGER
+#ifndef USER_MANAGER_H_INCLUDED
+#define USER_MANAGER_H_INCLUDED
+#include "classes/User.h"
 #include <string>
-#include <jsoncpp/json/json.h>
 
-
-/**
- * @brief Classe que contem dados do usuario
- * 
- */
-class User {
-    private:
-        int id;
-        std::string nome;
-        std::string email;
-        std::string senha;
-
+class UserManager {
     public:
-        User(int id, const std::string&nome,const std::string&email,const std::string&senha) {
-            this->id = id;
-            this->nome = nome;
-            this->email = email;
-            this->senha = senha;
-        }
+        /**
+         * @brief Carrega um usuario
+         * 
+         * Carrega os dados de um usuário.
+         * @return Ponteiro para usuário. Se o usuário não for encontrado, retorna ponteiro para nulo.
+         * 
+         */
+        static User* loadUser(const std::string&email);
 
-        int getId();
-        std::string getNome();
-        std::string getEmail();
-        std::string getSenha();
+        /**
+         * @brief verifica usuário e senha.
+         * 
+         * @return Se a senha for válida, retorna ponteiro para Usuário.
+         * Se a senha for inválida ou o usuário não for encontrado, retorna ponteiro para nulo.
+         */
+        static User* verifyLogin(const std::string&email, const std::string&senha);
 
-        bool verifyPassword(const std::string&senha);
+        /**
+         * @brief Registra um usuário
+         * 
+         * Registra um usuario com algumas informações.
+         * 
+         * @return verdadeiro em caso de sucesso.
+         */
+        static bool registerUser(const std::string&email, const std::string&nome, const std::string&senha);
+
+        /**
+         * @brief Remove usuario
+         * 
+         * @return verdadeiro em caso de sucesso.
+         */
+        static bool removeUser(const std::string&email);
 };
 
-namespace UserManager {
-
-    /**
-     * @brief Carrega um usuario
-     * 
-     * Carrega os dados de um usuário.
-     * 
-     * @param email 
-     * @return User* 
-     */
-    User* loadUser(const std::string&email);
-
-    /**
-     * @brief verifica usuário e senha.
-     * 
-     * Verifica se usuário esta cadastrado.
-     * 
-     * @param usuario 
-     * @param senha 
-     * @return std::string 
-     */
-    std::string verifyLogin(const std::string&email, const std::string&senha);
-
-    /**
-     * @brief Registra um usuário
-     * 
-     * Registra um usuario com algumas informações.
-     * 
-     * @param name 
-     * @param password 
-     * @param email 
-     * 
-     * @return std::string 
-     */
-    std::string registerUser(const std::string&email, const std::string&nome, const std::string&senha);
-
-    /**
-     * @brief Remove usuario
-     * 
-     * @param email 
-     *  
-     * @return std::string 
-     */
-    std::string removeUser(const std::string&email);
-};
-
-#endif//_USER_MANAGER
+#endif//USER_MANAGER_H_INCLUDED
--- backend/sources/UserManager.cpp
+++ backend/sources/UserManager.cpp
@@ -2,95 +2,42 @@
 #include "../headers/DatabaseModule.h"
 #include <iostream>
 
-std::string UserManager::verifyLogin(const std::string&email, const std::string&password) {
-    try {
-        DatabaseResult* result;
-        result = DatabaseModule::getInstance()->executeQuery("SELECT SENHA FROM `usuarios` WHERE EMAIL = '" + email + "'");
-        if (result && result->size() > 0) {   
-            auto element = result->front();
-            if (element["SENHA"] == password) {
-                return "VALID_PASSWORD";
-            }
-            else {
-                return "INVALID_PASSWORD";
-            }
-        } else {
-            return "UNREGISTERED_USER";
-        }
-    } catch (DatabaseError&e) {
-        return "DATABASE_ERROR:" + e.what();
-    }
-}
-
-std::string User::getNome() {
-    return this->nome;
-}
-
-std::string User::getEmail() {
-return this->email;
-}
-
-std::string User::getSenha() {
-return this->senha;
-}
-int User::getId() {
-return this->id;
-}
-
-bool User::verifyPassword(const std::string&senhaDigitada) {
-    return this->senha == senhaDigitada;
-}
-
-std::string UserManager::registerUser(const std::string&email, const std::string&nome, const std::string&senha) {
-    try {
-        if (UserManager::loadUser(email) == NULL){
-            DatabaseResult* result;
-            result = DatabaseModule::getInstance()->executeQuery("INSERT INTO `usuarios`(`ID`, `EMAIL`, `NOME`, `SENHA`) VALUES (NULL,'" + email + "','" + nome + "','" + senha + "')");
-            if (UserManager::loadUser(email)->getEmail() == email) {
-                return "SUCCESS";
-            }
-            else {
-                return "ERROR";
-            }
-        }
-        else {
-            return "REGISTERED_USER";
-        }
-    } catch (DatabaseError&e) {
-        return "DATABASE_ERROR:" + e.what();
-    }
-}
-
-std::string UserManager::removeUser(const std::string&email) {
-    try {
-        if (loadUser(email) != NULL){
-            DatabaseResult* result;
-            result = DatabaseModule::getInstance()->executeQuery("DELETE FROM `usuarios` WHERE EMAIL = '" + email + "'");
-            if (loadUser(email) == NULL) {
-                return "SUCCESS";
-            }
-            else {
-                return "ERROR";
-            }
-        }
-        else {
-            return "UNREGISTERED_USER";
-        }
-    } catch (DatabaseError&e) {
-        return "DATABASE_ERROR:" + e.what();
-    } 
-}
-
-
 User* UserManager::loadUser(const std::string&email) {
     DatabaseResult* result;
-    result = DatabaseModule::getInstance()->executeQuery("SELECT * FROM `usuarios` WHERE EMAIL = '" + email + "'");
-    if (result && result->size() > 0) {
-        auto element = result->front();
-        User *client = new User(std::stoi(element["ID"]), element["NOME"], element["EMAIL"], element["SENHA"]);;
-        return client;
-    }
-    else {
-        return NULL;
-    }
+
+    try {
+        result = DatabaseModule::getInstance()->executeQuery("SELECT * FROM `usuarios` WHERE `EMAIL` = '" + email + "'");
+
+        if (result != nullptr && result->size() > 0) {
+            auto element = result->front();
+            return new User(std::stoi(element["ID"]), element["NOME"], element["EMAIL"], element["SENHA"]);
+        }
+    } catch (const DatabaseError&err) { }
+    
+    return nullptr;
+}
+
+User* UserManager::verifyLogin(const std::string&email, const std::string&senha) {
+    User* user = loadUser(email);
+    if (user != nullptr && user->verifyPassword(senha))
+        return user;
+    return nullptr;
+}
+
+bool UserManager::registerUser(const std::string&email, const std::string&nome, const std::string&senha) {
+    if (!loadUser(email)) try {
+        DatabaseResult* result = DatabaseModule::getInstance()->executeQuery("INSERT INTO `usuarios`(`ID`, `EMAIL`, `NOME`, `SENHA`) VALUES (NULL,'" + email + "','" + nome + "','" + senha + "')");
+        return true;
+    } catch (const DatabaseError&err) { }
+
+    return false;
+}
+
+bool UserManager::removeUser(const std::string&email) {
+    if (loadUser(email)) try {
+        DatabaseResult* result = DatabaseModule::getInstance()->executeQuery("DELETE FROM `usuarios` WHERE `EMAIL` = '" + email + "'");
+        return result != NULL;
+    } catch (const DatabaseError&err) { }
+
+    return false;
 }
